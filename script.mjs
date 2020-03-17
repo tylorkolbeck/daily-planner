f0190bf853da47354f01a4c32376454465c35bb0
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-03-17 09:28:02 -0700

    refactored

--- script.mjs
+++ script.mjs
@@ -1,10 +1,18 @@
 let currentHour
 
+
+// Start the app
 init()
 
+
+/**
+ * Sets global current hour, initalizes the 
+ * time block UI styles and then starts an interval
+ * to check the time every minute
+ */
 function init() {
     currentHour = moment().hour()
-    updateBlockColors(currentHour)
+    updateTimeBlockUI()
     startTimeCheckInterval()
 }
 
@@ -15,24 +23,30 @@ function init() {
 function startTimeCheckInterval() {
     setInterval(() => {
         currentHour = moment().hour()
-        updateBlockColors(currentHour)
-    }, 1000)
+        updateTimeBlockUI()
+        // updateBlockColors(currentHour)
+    }, 60000)
 }
 
+
+
 /**
- * 
  * @param currentHour 
  * updates the time-block elements depending on whether or not 
  * the time is present, past or future time block
  */
-function updateBlockColors(currentHour) {
-    $('.time-block').removeClass('present past')
-    $('.time-block').each(function() {
-        let id = parseInt($(this).attr('id').split('-')[1])
-        if (id === currentHour) $(this).addClass('present')
-        if (id < currentHour) $(this).addClass('past')
+function updateTimeBlockUI() {
+    // Loop through the elements and determine which class needs to be applied
+    $('.time-block').each(function(index, el) {
+
+        // Remove currently applied classes
+        $(el).removeClass('present past')
+        let hourId = parseInt($(el).attr('id').split("-")[1])
+        if (hourId === currentHour) $(el).addClass('present')
+        if (hourId < currentHour) $(el).addClass('past')
     })
 }
+
 
 
 
